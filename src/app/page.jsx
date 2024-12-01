a0cb83ac030f76fb6d1e083ab222253370bc4a79
Ajoe62 <ijeborjoe@gmail.com>
2024-12-01 16:37:06 +0100

    pre-package install

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+//import Image from "next/image";
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div>
+      <h1>This is the home page</h1>
+      <Button>click me</Button>
+    </div>
+  )
+}
